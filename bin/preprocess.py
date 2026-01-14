3e0169060959c102ef5f1274a56755cf7f6f95f4
bolt3x <valeriofassi@hotmail.it>
2026-01-14 23:42:36 +0100

    🔧

--- bin/preprocess.py
+++ bin/preprocess.py
@@ -149,7 +149,6 @@ def apply_basic_correction(
         image, fov_size, overlap=0
     )
 
-    get_darkfield = False # Disable darkfield estimation for speed; can be enabled if needed
     basic = BaSiC(get_darkfield=get_darkfield, smoothness_flatfield=1)
 
     if autotune:
